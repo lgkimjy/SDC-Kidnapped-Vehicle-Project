d002274604193f3cde35a40ed98982bfcaff234f
lgkimjy <lgkimjy@naver.com>
2021-04-15 22:16:59 +0900

    deleted and removed dir

--- install-mac.sh
+++ install-mac.sh
@@ -4,10 +4,10 @@ cd uWebSockets
 git checkout e94b6e1
 patch CMakeLists.txt < ../cmakepatch.txt
 mkdir build
-export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig 
+export PKG_CONFIG_PATH=/opt/homebrew/Cellar/openssl@1.1/1.1.1k/lib/pkgconfig
 cd build
 OPENSSL_VERSION=`openssl version -v | cut -d' ' -f2`
-cmake -DOPENSSL_ROOT_DIR=$(brew --cellar openssl)/$OPENSSL_VERSION -DOPENSSL_LIBRARIES=$(brew --cellar openssl)/$OPENSSL_VERSION/lib ..
+cmake -DOPENSSL_ROOT_DIR=$(brew --cellar openssl)/1.1.1k -DOPENSSL_LIBRARIES=$(brew --cellar openssl)/1.1.1k/lib ..
 make 
 sudo make install
 cd ..
